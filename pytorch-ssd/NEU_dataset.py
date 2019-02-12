d1958eb92ef36ccc7c3483d8faa2f7aa196619da
TaehoLi <43288563+TaehoLi@users.noreply.github.com>
2019-02-13 01:31:24 +0900

    Update NEU_dataset.py

--- pytorch-ssd/NEU_dataset.py
+++ pytorch-ssd/NEU_dataset.py
@@ -4,10 +4,10 @@ import xml.etree.ElementTree as ET
 import cv2
 
 
-class VOCDataset:
+class NEUDataset:
 
-    def __init__(self, root, transform=None, target_transform=None, is_test=False, keep_difficult=True):# keep_difficult=False
-        """Dataset for VOC data.
+    def __init__(self, root, transform=None, target_transform=None, is_validate=False, keep_difficult=True):
+        """Dataset for NEU data.
         Args:
             root: the root of the VOC2007 or VOC2012 dataset, the directory contains the following sub-directories:
                 Annotations, ImageSets, JPEGImages, SegmentationClass, SegmentationObject.
@@ -15,11 +15,17 @@ class VOCDataset:
         self.root = pathlib.Path(root)
         self.transform = transform
         self.target_transform = target_transform
-        if is_test:
-            image_sets_file = self.root / "ImageSets/Main/test.txt"
+        
+        if is_validate:
+            image_sets_file = self.root / "val+noise.txt"
         else:
-            image_sets_file = self.root / "ImageSets/Main/trainval.txt"
-        self.ids = VOCDataset._read_image_ids(image_sets_file)
+            #if is_validate:
+            #    image_sets_file = self.root / "ImageSets/Main/val.txt"
+            #else:
+            #    image_sets_file = self.root / "ImageSets/Main/train.txt"
+            image_sets_file = self.root / "train+noise.txt"
+        
+        self.ids = NEUDataset._read_image_ids(image_sets_file)
         self.keep_difficult = keep_difficult
 
         self.class_names = ('BACKGROUND', 'crazing',
@@ -64,33 +70,53 @@ class VOCDataset:
         return ids
 
     def _get_annotation(self, image_id):
-        annotation_file = self.root / f"ANNOTATIONS/{image_id}.xml"
-        objects = ET.parse(annotation_file).findall("object")
-        boxes = []
-        labels = []
-        is_difficult = []
-        for object in objects:
-            class_name = object.find('name').text.lower().strip()
-            bbox = object.find('bndbox')
-            # VOC dataset format follows Matlab, in which indexes start from 0
-            x1 = float(bbox.find('xmin').text)# - 1
-            y1 = float(bbox.find('ymin').text)# - 1
-            x2 = float(bbox.find('xmax').text)# - 1
-            y2 = float(bbox.find('ymax').text)# - 1
-            boxes.append([x1, y1, x2, y2])
-            labels.append(self.class_dict[class_name])
-            is_difficult_str = object.find('difficult').text
-            is_difficult.append(int(is_difficult_str) if is_difficult_str else 0)
+        
+        try:
+            annotation_file = self.root / f"ANNOTATIONS/{image_id}.xml"
+            objects = ET.parse(annotation_file).findall("object")
+            boxes = []
+            labels = []
+            is_difficult = []
 
-        return (np.array(boxes, dtype=np.float32),
-                np.array(labels, dtype=np.int64),
-                np.array(is_difficult, dtype=np.uint8))
+            for object in objects:
+                class_name = object.find('name').text.lower().strip()
+                bbox = object.find('bndbox')
+
+                x1 = float(bbox.find('xmin').text)
+                y1 = float(bbox.find('ymin').text)
+                x2 = float(bbox.find('xmax').text)
+                y2 = float(bbox.find('ymax').text)
+
+                boxes.append([x1, y1, x2, y2])
+                labels.append(self.class_dict[class_name])
+                is_difficult_str = object.find('difficult').text
+                is_difficult.append(int(is_difficult_str) if is_difficult_str else 0)
+
+            a = np.array(boxes, dtype=np.float32)
+            b = np.array(labels, dtype=np.int64)
+            c = np.array(is_difficult, dtype=np.uint8)
+            if a.size==0 or b.size==0:
+                a = np.array([[0,0,0,0]], dtype=np.float32)
+                b = np.array([0], dtype=np.int64)
+                c = np.array([0], dtype=np.uint8)
+                return (a,b,c)
+            else:
+                pass
+            return (a,b,c)
+        except FileNotFoundError:
+            a = np.array([[0,0,0,0]], dtype=np.float32)
+            b = np.array([0], dtype=np.int64)
+            c = np.array([0], dtype=np.uint8)
+            return (a,b,c)
 
     def _read_image(self, image_id):
-        image_file = self.root / f"IMAGES_COLOR/{image_id}.jpg"
-        image = cv2.imread(str(image_file))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        try:
+            image_file = self.root / f"IMAGES_COLOR/{image_id}.jpg"
+            image = cv2.imread(str(image_file), 1)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        except:
+            image_file = self.root / f"IMAGES_distorted/{image_id}.jpg"
+            image = cv2.imread(str(image_file), 1)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         return image
-
-
 
