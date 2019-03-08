ff8eacfaf53f24b66de4421e159940b42eda9997
TaehoLi <43288563+TaehoLi@users.noreply.github.com>
2019-03-08 17:01:57 +0900

    Update vgg_ssd.py

--- lab/ssd/vgg_ssd.py
+++ lab/ssd/vgg_ssd.py
@@ -10,10 +10,10 @@ from nn.vgg import vgg
 
 from ssd.ssd import SSD
 from ssd.predictor import Predictor
-from ssd.config import vgg_ssd_config as config
+#from ssd.config import vgg_ssd_config as config
 
 
-def create_vgg_ssd(num_classes, device=None, is_test=False):
+def create_vgg_ssd(num_classes, config_name, device=None, is_test=False):
     vgg_config = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
                   512, 512, 512]
     base_net = ModuleList(vgg(vgg_config))
@@ -66,13 +66,27 @@ def create_vgg_ssd(num_classes, device=None, is_test=False):
         Conv2d(in_channels=256, out_channels=4 * num_classes, kernel_size=3, padding=1),
         Conv2d(in_channels=256, out_channels=4 * num_classes, kernel_size=3, padding=1), # TODO: change to kernel_size=1, padding=0?
     ])
-
+    
+    if config_name == "vgg_ssd_config_VOC_300":
+        from ssd.config import vgg_ssd_config_VOC_300 as config
+    elif config_name == "vgg_ssd_config_COCO_512":
+        from ssd.config import vgg_ssd_config_COCO_512 as config
+    else:
+        return print("There is no config file.")
     return SSD(num_classes, base_net, source_layer_indexes,
                extras, classification_headers, regression_headers,
                is_test=is_test, config=config, device=device)
 
 
-def create_vgg_ssd_predictor(net, candidate_size=200, nms_method=None, sigma=0.5, device=None):
+def create_vgg_ssd_predictor(net, config_name, candidate_size=200, nms_method=None, sigma=0.5, device=None):
+    
+    if config_name == "vgg_ssd_config_VOC_300":
+        from ssd.config import vgg_ssd_config_VOC_300 as config
+    elif config_name == "vgg_ssd_config_COCO_512":
+        from ssd.config import vgg_ssd_config_COCO_512 as config
+    else:
+        return print("There is no config file.")
+    
     predictor = Predictor(net, config.image_size, config.image_mean,
                           nms_method=nms_method,
                           iou_threshold=config.iou_threshold,
